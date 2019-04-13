ce0f3013554949b621d74122a421d0f4de6a7cb5
caoyixiong <caoyixiong001@lianjia.com>
2019-04-13 14:29:15 +0800

    Update 反转链表II.java

--- src/middle/链表/反转链表II.java
+++ src/middle/链表/反转链表II.java
@@ -6,6 +6,7 @@ package middle.链表;
  * @Copyright (c) 2015, lianjia.com All Rights Reserved
  * <p>
  * 反转从位置 m 到 n 的链表。请使用一趟扫描完成反转。
+ * 1 ≤ m ≤ n ≤ 链表长度。
  * <p>
  * 输入: 1->2->3->4->5->NULL, m = 2, n = 4
  * 输出: 1->4->3->2->5->NULL
